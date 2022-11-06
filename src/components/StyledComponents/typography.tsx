569f478656de17c8d746a83fd7ca66735c553ec2
Tung Khuat <khuathanhtung09@gmail.com>
2022-11-07 01:47:53 +0200

    feat: added common typography styled components

---
+++ src/components/StyledComponents/typography.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const PageTitle = styled.header`
+   font-size: 56px;
+   line-height: 40px;
+`
+
+export const Subtitle = styled.p`
+   color: var(--color-text-muted);
+`
+
+export const StyledLink = styled.a`
+   cursor: pointer;
+   color: var(--color-text-highlight);
+`
