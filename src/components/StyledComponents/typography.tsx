799902d0bce006833f4a125d21ca2cdf8b52e857
Tung Khuat <khuathanhtung09@gmail.com>
2022-11-08 13:49:20 +0200

    style: added new font family and change highlight color

--- src/components/StyledComponents/typography.tsx
+++ src/components/StyledComponents/typography.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const PageTitle = styled.header`
-   font-size: 56px;
+   font-size: 52px;
    line-height: 40px;
+   font-weight: 600;
 `
 
 export const Subtitle = styled.p`
