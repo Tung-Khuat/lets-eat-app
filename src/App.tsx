1742db6ac0db24ee69a198f14e3702aa28b51727
Tung Khuat <khuathanhtung09@gmail.com>
2022-11-08 12:49:06 +0200

    feat: implemented react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import FullViewLoading from './components/feedback/loadingIndicators/FullViewLoading'
+import routes from './routes'
+interface IRoute {
+   title: string
+   path: string
+   component: JSX.Element
+   disabled: boolean
+   isPublic: boolean
+   category: string
+}
+type createRouteElementFunc = (route: IRoute, index: any) => any
+
 function App() {
-   return <>Hello World</>
+   // Routing
+   const createRouteElement: createRouteElementFunc = (
+      route: IRoute,
+      index
+   ) => {
+      const routeProps = {
+         index,
+         key: route.path,
+         path: route.path,
+         element: route.component,
+      }
+
+      return <Route {...routeProps} />
+   }
+
+   const routeElements = routes.map(createRouteElement)
+   const renderRoute = () => {
+      return (
+         <>
+            <Routes>{routeElements}</Routes>
+         </>
+      )
+   }
+
+   return (
+      <Suspense fallback={<FullViewLoading />}>
+         <Router>{renderRoute()}</Router>
+      </Suspense>
+   )
 }
 
 export default App
